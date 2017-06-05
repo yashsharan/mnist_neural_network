c6554995b78d4eb5afeea628d38cdc75530ac144
yashsharan <yashsharan.0805@gmail.com>
2017-06-05 20:38:38 +0530

    added biases and activation fuction to both the convolutional layer'

--- CNN_mnsit.py
+++ CNN_mnsit.py
@@ -28,10 +28,10 @@ def conv_nerual_network(x):
 
 	x=tf.reshape(x,shape=[-1,28,28,1])
 
-	conv1=conv2d(x,weights['W_conv1'])
+	conv1=tf.nn.relu(conv2d(x,weights['W_conv1'])+biases['b_conv1'])
 	conv1=maxpool2d(conv1)
 
-	conv2=conv2d(conv1,weights['W_conv2'])
+	conv2=tf.nn.relu(conv2d(conv1,weights['W_conv2'])+biases['b_conv2'])
 	conv2=maxpool2d(conv2)
 
 	fc=tf.reshape(conv2,[-1,7*7*64])
